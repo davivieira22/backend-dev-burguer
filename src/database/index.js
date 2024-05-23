2dc2d4a47d0ba1626e4fe392f12de46f1a782a98
davivieira22 <nftdavidavila22@gmail.com>
2024-05-22 21:37:06 -0300

    devburguer

--- src/database/index.js
+++ src/database/index.js
@@ -3,6 +3,7 @@ import ConfigDatabase from '../config/database'
 import User from '../app/models/User';
 import Product from '../app/models/product';
 import Category from '../app/models/Category';
+import mongoose from 'mongoose';
 
 const models = [User, Product, Category];
 
@@ -10,11 +11,17 @@ class Database {
    constructor() {
 
       this.init();
+      this.mongo();
    }
    init() {
       this.connection = new Sequelize(ConfigDatabase);
       models.map((model) => model.init(this.connection)).map(
          (model) => model.associate && model.associate(this.connection.models),);
+   }
+   mongo() {
+      this.mongoConnection = mongoose.connect(
+         'mongodb://localhost:27017/devburger',
+      );
    }
 }
 
