0b309ff3bf766d793ef114250ce1442dd519013e
davivieira22 <nftdavidavila22@gmail.com>
2024-06-01 23:51:56 -0300

    devburger

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import express from 'express'
 import routes from './Routes';
 import './database';
 import {resolve} from 'node:path';
+import cors from 'cors'
+
 class App {
     constructor() {
         this.app = express()
+        this.app.use(cors());
         this.middlewares();
         this.routes();
+       
     }
     middlewares() {
         this.app.use(express.json());
